d225e0a92a32bb43888ec7c1db76dbc46d381e33
healer-1205 <crazyhealer1205@gmail.com>
2022-09-05 06:54:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
