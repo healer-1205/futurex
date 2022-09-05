380604486ca99ad043888bad32a7c5bffa203fb1
healer-1205 <crazyhealer1205@gmail.com>
2022-09-05 12:28:43 -0400

    finish navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
-import Logo from "assets/logo.svg";
+import "scss/NavBar.scss";
+import Logo from "assets/images/navbar/logo.svg";
+import searchIcon from "assets/images/navbar/searchIcon.svg";
+import profileIcon from "assets/images/navbar/profileIcon.svg";
 
 function NavBar() {
   const [click, setClick] = useState(false);
@@ -9,7 +11,7 @@ function NavBar() {
   const handleClick = () => setClick(!click);
   return (
     <>
-      <nav className="navbar">
+      <div className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
             <img src={Logo} alt="Logo" />
@@ -62,14 +64,22 @@ function NavBar() {
             </li>
           </ul>
 
-          <div className="serchBoxArea">
+          <div className="searchBoxArea">
+            <img src={searchIcon} alt="searchIcon" className="searchIcon" />
             <input type="text" placeholder="some text in ther" className="searchBox" />
           </div>
+
+          <div className="profile">
+            <img src={profileIcon} alt="profile" className="profileIcon" />
+          </div>
+
+          <button className="postBtn">Post a Job</button>
+
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
         </div>
-      </nav>
+      </div>
     </>
   );
 }
