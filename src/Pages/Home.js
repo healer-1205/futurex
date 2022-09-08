f136d713855c099080e3025cce10e2e2e67ea444
healer-1205 <crazyhealer1205@gmail.com>
2022-09-08 05:12:41 -0400

    make blog part in homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import "scss/home.scss";
 import { BasicButton } from "components/Button/BasicButton";
+import { Blog } from "components/Blog";
+import { Connection } from "components/Connection"
 import BackgroundTop from "assets/images/background/backgroundTop.svg";
 import ProfileIcon from "assets/images/homepage/profileIcon.svg";
 import Edit from "assets/images/utils/editIcon.svg";
 import Line from "assets/images/utils/lineIcon.svg";
 import ImageIcon from "assets/images/utils/imageIcon.svg";
 import ShareIcon from "assets/images/utils/shareIcon.svg";
+import Avatar from "assets/images/utils/avatar.svg";
 
 const Wrapper = styled.div`
   background: url(${(props) => props.bgImg}) no-repeat center center fixed;
@@ -15,6 +19,9 @@ const Wrapper = styled.div`
 `;
 
 export const Home = () => {
+
+  const [tabState, setTabState] = useState("forYou");
+
   return (
     <Wrapper bgImg={BackgroundTop} className="pt-7 lg:px-20 md: px-5">
       <div className="grid lg:grid-cols-4 grid-cols-1">
@@ -67,7 +74,7 @@ export const Home = () => {
             </BasicButton>
           </div>
         </div>
-        <div className="lg:col-span-2 sm:col-span-1 mx-auto blog">
+        <div className="lg:col-span-2 sm:col-span-1 mx-auto w-full blog">
           <div className="searchBox">
             <img src={ProfileIcon} alt="ProfileIcon" className="profileIcon" />
             <div className="mindArea">
@@ -84,31 +91,61 @@ export const Home = () => {
               Share your hiring
             </button>
           </div>
-        </div>
-        <div className="blog__mobile">
-          <div className="flex justify-center items-center pt-3">
-            {/* <img src={ProfileIcon} alt="ProfileIcon" className="profileIcon" /> */}
-            <div className="mindArea">
-              <div className="mindArea__inner">
-                <input type="text" placeholder="What's on your mind?" className="inputField" />
+          <div className="searchBox__mobile">
+            <div className="flex justify-center items-center pt-3">
+              {/* <img src={ProfileIcon} alt="ProfileIcon" className="profileIcon" /> */}
+              <div className="mindArea">
+                <div className="mindArea__inner">
+                  <input type="text" placeholder="What's on your mind?" className="inputField" />
+                </div>
               </div>
             </div>
+            <div className="flex justify-center items-center pt-3">
+              <button className="_button flex items-center justify-center">
+                <img src={ImageIcon} alt="ImageIcon" width="20" height="20" className="mr-1" />
+                Add image
+              </button>
+            </div>
+            <div className="flex justify-center items-center pt-3 pb-3">
+              <button className="_button flex items-center justify-center">
+                <img src={ShareIcon} alt="ShareIcon" width="20" height="20" className="mr-1" />
+                Share your hiring
+              </button>
+            </div>
           </div>
-          <div className="flex justify-center items-center pt-3">
-            <button className="_button flex items-center justify-center">
-              <img src={ImageIcon} alt="ImageIcon" width="20" height="20" className="mr-1" />
-              Add image
-            </button>
+
+          <div className="bg-[#1C1B2E] mt-5 grid grid-cols-3 rounded-sm h-11 justify-center items-center">
+            <button onClick={() => setTabState("forYou")} className={`font-['Poppins'] non-italic font-medium text-[14px] leading-6 text-center  mix-blend-normal opacity-80 ${tabState === 'forYou' ? 'text-[#9A66FF]' : 'text-white'}`}>For you</button>
+            <button onClick={() => setTabState("following")} className={`font-['Poppins'] non-italic font-medium text-[14px] leading-6 text-center  mix-blend-normal opacity-80 ${tabState === 'following' ? 'text-[#9A66FF]' : 'text-white'}`}>Following</button>
+            <button onClick={() => setTabState("recent")} className={`font-['Poppins'] non-italic font-medium text-[14px] leading-6 text-center  mix-blend-normal opacity-80 ${tabState === 'recent' ? 'text-[#9A66FF]' : 'text-white'}`}>Recent</button>
           </div>
-          <div className="flex justify-center items-center pt-3">
-            <button className="_button flex items-center justify-center">
-              <img src={ShareIcon} alt="ShareIcon" width="20" height="20" className="mr-1" />
-              Share your hiring
-            </button>
+          <div className="mt-5 rounded bg-[#1C1B2E] px-4 py-6">
+            {
+              tabState === "forYou" &&
+              <Blog avatar={Avatar} author="Wendy Zheng" intro="One-line introintro intro xxxxxx" date="Today at 9:03 AM" like={11} comment={9}>
+                I am a recent college graduate looking to break into the world of Web3.Bring the professional profile ownership back to the users and take down Linkedin!I am a recent college graduate looking to break into the world of Web3.Bring the professional profile ownership back to the users and take down Linkedin!
+              </Blog>
+            }
+            {
+              tabState === "following" &&
+              <div>
+                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classNamees to control the content visibility and styling.</p>
+              </div>
+            }
+            {
+              tabState === "recent" &&
+              <div>
+                <p className="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classNamees to control the content visibility and styling.</p>
+              </div>
+            }
           </div>
+
         </div>
         <div className="suggested col-span-1">
-          <p className="suggested__title">Suggested connection</p>
+          <p className="suggested__title mx-5">Suggested connection</p>
+          <div className="mt-5 grid grid-cols-2">
+            <Connection name="Bob Zhang" job="Data Analyst at IQVIA" />
+          </div>
         </div>
       </div>
     </Wrapper>
---
+++ src/components/Blog/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import FollowIcon from "assets/images/utils/followIcon.svg";
+import LikeIcon from "assets/images/utils/likeIcon.svg";
+import CommentIcon from "assets/images/utils/commentIcon.svg";
+import ContactIcon from "assets/images/utils/contactIcon.svg";
+import ShareIcon from "assets/images/utils/shareIcon.svg";
+import "scss/blog.scss";
+
+export const Blog = (props) => {
+	return (
+		<div className="flex flex-col">
+			<div className="flex justify-between blogHead">
+				<div className="flex">
+					<img src={props.avatar} alt="Avatar" width="66" height="66" />
+					<div className="flex flex-col ml-4">
+						<p className="blogHead__author">{props.author}</p>
+						<p className="blogHead__intro">{props.intro}</p>
+						<p className="blogHead__date">{props.date}</p>
+					</div>
+				</div>
+				<div className="flex justify-center items-center">
+					<p className="font-['Poppins'] non-italic font-semibold text-sm text-center text-[#7af7ff]">Follow</p>
+					<img src={FollowIcon} alt="Follow" width="15" height="15" />
+				</div>
+			</div>
+			<div className="pt-5 blogBody">
+				<p className="blogBody__content">
+					{props.children}
+				</p>
+			</div>
+			<div className="mt-5 pt-5 blogFooter">
+				<div className="flex gird-cols-4 items-center justify-around">
+					<div className="flex">
+						<p className="blogFooter__title pr-2">Like&nbsp;{props.like}</p>
+						<img src={LikeIcon} alt="like" width="16" height="16" />
+					</div>
+					<div className="flex">
+						<p className="blogFooter__title pr-2">Comment&nbsp;{props.comment}</p>
+						<img src={CommentIcon} alt="comment" width="16" height="16" />
+					</div>
+					<div className="flex">
+						<img src={ContactIcon} alt="contact" width="16" height="16" />
+					</div>
+					<div className="flex">
+						<p className="blogFooter__title pr-2">Share&nbsp;</p>
+						<img src={ShareIcon} alt="share" width="16" height="16" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
